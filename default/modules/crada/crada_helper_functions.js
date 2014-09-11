d95180bde7cfd22e64d2bd2dadcdf0d75de5b542
genesis55 <chris.kneisler@nih.gov>
2014-09-11 15:02:58 -0400

    Added Madlib search and replace to "Make New Document"

--- default/modules/crada/crada_helper_functions.js
+++ default/modules/crada/crada_helper_functions.js
@@ -16,3 +16,19 @@ function display_error(response, status, error) {
     var errorMessage = error || response.statusText;
     alert(errorMessage);
 }
+
+jQuery.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    jQuery.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
