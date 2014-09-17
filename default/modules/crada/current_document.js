94123a76b0aac363ccb53f98074310c202c0582c
genesis55 <chris.kneisler@nih.gov>
2014-09-17 16:54:07 -0400

    Added clause and annotate dialog functionality.

--- default/modules/crada/current_document.js
+++ default/modules/crada/current_document.js
@@ -3,12 +3,17 @@ var documentSections;
 $(document).ready(function () {
 	create_dialogs();
 
-	
+	$("#current_annotation_content").tooltip({show: {delay: 350}}); /*Adding jQuery tooltip for annotations*/
 	$("#load_button").click(click_load_button);
 	$("#save_button").click(click_save_button);
 	$("#select_document_button").click(click_select_document_button);
 	$("#annotation_options").change(change_annotation_options);
-
+ 	$("#current_document_content").on( "click", "p", function(event) {
+            editClause(event);
+	});
+ 	$("#current_annotation_content").on( "click", "p", function(event) {
+            editAnnotation(event);
+	});
 	var querystring = getQueryString();
 	if (querystring["action"] == 'Load') {
 		document_id = querystring["document_id"];
@@ -21,6 +26,57 @@ $(document).ready(function () {
 //  	$('#annotator').annotator().anotator('setupPlugins');
 
 });
+
+function editClause(e) {
+	var ref = e.target.id;
+	$("#"+ref).attr("contenteditable", "true").addClass('edit-section');
+	var data_clause = $("#"+ref).attr("data-clause");
+	var content;
+	content = '<textarea rows="5" cols="90" class="data_clause_textarea">';
+	content += data_clause;
+	content += '</textarea>';
+	$(content).dialog({
+		resizable: false,
+		height:430,
+		width:700,
+		modal: true,
+		title: $("#"+ref).attr("dialog-title"),
+		buttons: {
+			"Cancel": function() {
+				$( this ).dialog( "close" );
+			},
+			Save: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	});		
+}
+
+function editAnnotation(e) {
+	var ref = e.target.id;
+
+	var data_annotate = $("#"+ref).attr("data-annotate");
+	var content;
+
+	content = '<textarea rows="5" cols="90" class="data_clause_textarea">';
+	content += data_annotate;
+	content += '</textarea>';
+	$(content).dialog({
+		resizable: false,
+		height:430,
+		width:700,
+		modal: true,
+		title: $("#"+ref).attr("dialog-title"),
+		buttons: {
+			"Cancel": function() {
+				$( this ).dialog( "close" );
+			},
+			Save: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	});		
+}
 
 function change_annotation_options() {
 	var annotationOption = $( "#annotation_options" ).val();
@@ -85,6 +141,7 @@ function click_select_document_button() {
 	var version = 0;
 	
 	ajax_caller("get_full_document", {'document_id':document_id, 'version':version}, get_document_elements_callback);
+	$("#load_dialog").dialog( "close" );
 	
 }
 
@@ -96,35 +153,90 @@ function get_document_elements_callback(data) {
 	var current_section = "";
 	var confidential_annotation;
 	var public_annotation;
-	var annotationLabel;
+	var annotation_footnote;
+	var section_reference;
+	var section_number = 0;
+	var data_clause;
 	for (var i=0; i<data.clauses.length; i++) {
 		if (!(data.clauses[i].text == 'silent') ) {
 			if (data.clauses[i].section != current_section) {
 				$("#current_document_content").append("<br /><br />");
 				$("#current_document_content").append($("<h2>").append(data.clauses[i].section));
 				current_section = data.clauses[i].section;
+				section_number++;
 			}
+			section_reference = section_number+"."+(i+1);
+			//data_clause = ​typeof data.clauses[i].text;
+			//alert(data_clause);
+			$("#current_document_content")
+				.append(
+					$("<p>")
+						.append("<sup class='annotation_footnote'>["+section_reference+"]</sup> "+data.clauses[i].text)
+						.addClass('contract_clause')
+						.attr("id", "clause-" + i)
+						.attr("dialog-title", "Section ["+section_reference+"]")
+						.attr("title", "Click to edit section")
+						.attr("data-clause", data.clauses[i].text)
+					);
 
-			$("#current_document_content").append($("<a href='#'>").append("<p><sup class='annotation_footnote'>"+(i+1)+".</sup> "+data.clauses[i].text+"</p>").addClass('clause').attr( "id", "clause-" + i));
-
+			//addAnnotationDiv(data.clauses[i].confidential_annotation, section_reference, 'Confid');
+			addAnnotationDiv("test", section_reference, "Confid");
+			//addAnnotationDiv(data.clauses[i].public_annotation, section_reference, 'Pub');
 			if( JSON.stringify(data.clauses[i].confidential_annotation).length > 2) {
+				var annotate = data.clauses[i].confidential_annotation;
+				annotate = $("<div/>").html(annotate).text();
 				data.clauses[i].confidential_annotation_short = trimAnnoation(JSON.stringify(data.clauses[i].confidential_annotation));
-				annotationLabel = "<b>Comment [Conf"+(i+1)+"]:  </b>";
-				$("#current_annotation_content").append($("<p>").append(annotationLabel+data.clauses[i].confidential_annotation_short).addClass('annotation').addClass('confidential_annotation'));	
+				annotation_footnote = "Comment [Conf"+section_reference+"]: ";
+				//addAnnotationDiv(data.clauses[i].confidential_annotation, 'Confid', "current_annotation_content");
+				$("#current_annotation_content")
+					.append(
+						$("<p class='conatract_clause'>")
+							.append( "<b>" + annotation_footnote + "</b> " + data.clauses[i].confidential_annotation_short)
+							.addClass('annotation')
+							.addClass('confidential_annotation')
+							.attr("id", "annotate-"+i+"annotateNeedsIndex")
+							.attr("data-annotate", annotate)
+							.attr("title", data.clauses[i].confidential_annotation)
+							.attr("dialog-title", annotation_footnote)
+					);	
 			}
 			if( JSON.stringify(data.clauses[i].public_annotation).length > 2) {
+				var annotate = data.clauses[i].public_annotation;
+				annotate = $("<div/>").html(annotate).text();
 				data.clauses[i].public_annotation_short = trimAnnoation(JSON.stringify(data.clauses[i].public_annotation));
-				annotationLabel = "<b>Comment [Pub"+(i+1)+"]:  </b>";
-				$("#current_annotation_content").append($("<p>").append(annotationLabel+data.clauses[i].public_annotation_short).addClass('annotation').addClass('public_annotation'));
+				annotation_footnote = "Comment [Pub"+(i+1)+"]:";
+				$("#current_annotation_content")
+					.append(
+						$("<p>")
+							.append("<b>" + annotation_footnote + "</b> " + data.clauses[i].public_annotation_short)
+							.addClass('annotation')
+							.addClass('public_annotation')
+							.attr("id", "annotate-"+i+"annotateNeedsIndex")
+							.attr("data-annotate", annotate)
+							.attr("title", data.clauses[i].public_annotation)
+							.attr("dialog-title", annotation_footnote)
+
+						);
 			}
 		} 
 	}
 	documentSections = data;
 	console.info("documentSections")
 	console.dir(documentSections);
+	$( ".annotation" ).tooltip({ track: true });
+
 }
+
+function addAnnotationDiv(annotation_data, section_reference, annotation_type) {
+	var annotations;
+	//split the annotation data on excel return
+	//annotations = JSON.stringify(annotation_data);
+	//console.log("section_reference: ");
+	console.dir(annotation_data);
+}
+
 function trimAnnoation(annotation) {
-	//If length is greater than 60 then trim at then find next previous space and trim
+	//If length is greater than 60 then find next previous space and trim
 	var max_size = 60;
 	if(annotation.length > max_size) {
 		//find the next previous space
