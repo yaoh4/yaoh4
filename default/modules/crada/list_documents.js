01fbe0e1757082a62af79cbfd32488edf1364bad
genesis55 <chris.kneisler@nih.gov>
2015-01-16 17:37:38 -0500

    Finished document search, set timeout 90, many bug fixes for QA2

--- default/modules/crada/list_documents.js
+++ default/modules/crada/list_documents.js
@@ -5,35 +5,63 @@ $(document).ready(function ($) {
   //
   // Check if a search term exists in the cookie
   //
+
   var search_term = getCookie("Drupal.visitor.document.searchterm");
   if (search_term.length > 0) {
     // Load the search term
     $("#search-term").val(search_term);
-    filterDocumentList();
+    updateDocumentList();
   }
 });
 
-function filterDocumentList() {
+function updateDocumentList() {
+
+  var search_term = $("#search-term").val();
+  setCookie("Drupal.visitor.document.searchterm", search_term, 1);
+  //
+  //Send the searchtem to server to get a list of document_id to show.
+  //
+  ajax_caller('search_clauses', {'searchterm':getCookie("Drupal.visitor.document.searchterm")}, search_clauses_callback);
+
+  console.log("Auto complete: "+search_term);
 
 }
 
-function updateDocumentList() {
-  //alert('search');
-  var search_term = $("#search-term").val();
-  setCookie("Drupal.visitor.document.searchterm", search_term, 1);
+function search_clauses_callback(data) {
 
-  if(search_term.length == 0) {
+  console.log("search_clauses_callback");
+  console.dir(data);
+  var count = 0;
+  $('#list-document > tbody  > tr').each(function(index) {
+    var target_id = $( this ).attr('id');
+    console.log( index + ": " + target_id );
+    if($.inArray(target_id, data.filter) == -1) {
+      console.log(target_id+" is NOT in filter");
+      $(this).hide();
+    } else {
+      console.log(target_id+" is in filter");
+      $(this).removeClass();
+      if(count % 2 ==0) {
+        $(this).addClass('even');
+      } else {
+        $(this).addClass('odd');
+      }
+      $(this).show();
+      count++;
+    }
+  });
+
+
+// Highlight the search input box
+  if(data.searchterm.length == 0) {
     $("#search-term").removeClass('searching');
-    $("#9").hide();
+    //$("#9").hide();
 
   } else {
     $("#search-term").addClass('searching');
-    $("#9").show();
+    //$("#9").show();
   }
 
-  console.log("Auto complete");
-  console.log($("#search-term").text());
-  console.log($("#search-term").val());
 
 }
 
