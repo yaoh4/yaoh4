7efa25ff40e9fae7dbcb9afaec26b8ecc8e2a516
genesis55 <chris.kneisler@nih.gov>
2015-02-05 16:22:51 -0500

    Added Validation check and stub for survivability statement on document.

--- default/modules/crada/crada_helper_functions.js
+++ default/modules/crada/crada_helper_functions.js
@@ -168,57 +168,6 @@ function replaceValidationUI2( form ) {
         }
     });
 }
-function initFormValidator(parent) {
-    // Replace the validation UI for all forms
-    console.log("initFormValidator");
-    //jQuery('#'+parent).hide();
-    //alert('Should be hidden');
-    var forms = document.getElementById(parent).querySelectorAll( "form" );
-    console.dir(forms);
-    for ( var i = 0; i < forms.length; i++ ) {
-        console.dir(forms[i]);
-        replaceValidationUI( forms[ i ] );
-    }
-}
-
-function replaceValidationUI( form ) {
-    // Suppress the default bubbles
-    form.addEventListener( "invalid", function( event ) {
-        event.preventDefault();
-    }, true );
-
-    // Support Safari, iOS Safari, and the Android browser—each of which do not prevent
-    // form submissions by default
-    form.addEventListener( "submit", function( event ) {
-        if ( !this.checkValidity() ) {
-            event.preventDefault();
-        }
-    });
-
-    var submitButton = form.querySelector( "button:not([type=button]), input[type=submit]" );
-    submitButton.addEventListener( "click", function( event ) {
-        var invalidFields = form.querySelectorAll( ":invalid" ),
-            errorMessages = form.querySelectorAll( ".error-message" ),
-            parent;
-
-        // Remove any existing messages
-        for ( var i = 0; i < errorMessages.length; i++ ) {
-            errorMessages[ i ].parentNode.removeChild( errorMessages[ i ] );
-        }
-
-        for ( var i = 0; i < invalidFields.length; i++ ) {
-            parent = invalidFields[ i ].parentNode;
-            parent.insertAdjacentHTML( "beforeend", "<div class='error-message'>" +
-                invalidFields[ i ].validationMessage +
-                "</div>" );
-        }
-
-        // If there are errors, give focus to the first invalid field
-        if ( invalidFields.length > 0 ) {
-            invalidFields[ 0 ].focus();
-        }
-    });
-}
 
 function generic_callback(data) {
 //  generic_callback for ajax_caller();
