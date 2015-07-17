c782a9c265bf6e9f7d57ec9f6ff55115483ff686
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2015-07-17 14:07:25 -0400

    Adding sanitation to url with secure cookies

--- default/modules/crada/crada_helper_functions.js
+++ default/modules/crada/crada_helper_functions.js
@@ -49,8 +49,15 @@ jQuery.fn.serializeObject = function() {
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    console.log('window.location.protocol: ' + window.location.protocol);
+
+    if(window.location.protocol == 'http:') {
+        document.cookie = cname + "=" + cvalue + "; " +  expires;
+    } else {
+        document.cookie = cname + "=" + cvalue + "; " +  expires + ";secure";
+    }
 }
 
 function getCookie(cname) {
